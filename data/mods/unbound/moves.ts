81ea51b7c1cd45dde2408ea333d76e63c778c6f1
richnanney <richardpnanney@gmail.com>
2023-10-04 11:07:39 -0600

    Added/updated a few moves.
    rocksmash -- now always lowers defense
    leech life -- back to base 20 power
    feint -- 50 power
    meteor mash -- 100 power
    snore -- 80 power
    fly -- 100 power
    dive -- 100 power
    glaciate -- 75 power, 100% accuracy
    dark void -- 80% accuracy
    smog -- 100% accuracy
    leech fang -- modern leech life + strong jaw boost

--- data/mods/unbound/moves.ts
+++ data/mods/unbound/moves.ts
@@ -1,14 +1,75 @@
 export const Moves: {[k: string]: ModdedMoveData} = {
+	rocksmash: {
+		inherit: true,
+		basePower: 50,
+		secondary: {
+			chance: 100,
+			boosts: {
+				def: -1,
+			},
+		},
+		desc: "100% chance of lowering the target's Defense by one stage.",
+		shortDesc: "Lowers the target's Def by 1 stage.",
+	},
+	leechlife: {
+		inherit: true,
+		basePower: 20,
+	},
+	feint: {
+		inherit: true,
+		basePower: 50,
+	},
+	meteormash: {
+		inherit: true,
+		basePower: 100,
+	},
+	snore: {
+		inherit: true,
+		basePower: 80,
+	},
+	fly: {
+		inherit: true,
+		basePower: 100,
+	},
+	dive: {
+		inherit: true,
+		basePower: 100,
+	},
+	glaciate: {
+		inherit: true,
+		basePower: 75,
+		accuracy: 100,
+	},
+	darkvoid: {
+		inherit: true,
+		accuracy: 80,
+	},
+	smog: {
+		inherit: true,
+		accuracy: 100,
+	},
+	leechfang: {
+		accuracy: 100,
+		basePower: 80,
+		category: "Physical",
+		desc: "The user strikes with a fang, and heals for a portion of the damage dealt.",
+		shortDesc: "Deal damage, heal a portion of it.",
+		name: "Leech Fang",
+		gen: 7,
+		pp: 10,
+		priority: 0,
+		flags: {bite: 1, contact: 1, heal:1},
+		drain: [1, 2],
+		secondary: null,
+		target: "normal",
+		type: "Bug",
+		contestType: "Clever",
+	},
+
+
 	/*
 	TODO:
 	Attack Powers:
-	Leech Life: 20
-	Feint: 50
-	Meteor Mash: 100
-	Snore: 80
-	Fly: 100
-	Dive: 100
-	Glaciate: 75
 	Moves With Power nerfed to 85 as of XY: 90
 	Moves With Power nerfed to 90 as of XY: 95
 	Moves With Power nerfed to 95 as of XY: 100
@@ -17,14 +78,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 	PLA Attacks: Altered based on similar moves -- Figure out what this means.
 	Steelyhit
 
-	Attack Accuracies:
-	Dark Void - 80%
-	Smog - 100%
-	Glaciate - 100%
-
-	Leech Fang is equivalent to Gen 7's Leech Life (and gets a Strong Jaw boost)
 	King's Shield lowers Attack by 2 on contact (Gen 7)
-	Rock Smash always lowers Defense
 	There is no money cap on G-Max Gold Rush
 	New PLA attacks with increased action speed are priority moves
 	Victory Dance is physical Quiver Dance
